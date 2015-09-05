e6be5322d1266bcb84ffe493f82b55f3142b990f
Tomoyuki KAMIYA <kamiya7140@gmail.com>
2015-09-05 17:33:02 +0900

    fix user model to add api to record fix_property_history

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
 	has_many :plans
+	has_many :property_fix_histories
+	
+	def fix_property(new_property)
+		# TODO: these should be executed atmicly
+		PropertyFixHistory.create(user: self, new_property: new_property)
+		self.update(property: new_property)
+	end
+
 end

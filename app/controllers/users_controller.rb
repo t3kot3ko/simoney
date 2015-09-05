8ae0d891a8569b3b981c1580dc92ce01bd55454d
Tomoyuki KAMIYA <kamiya7140@gmail.com>
2015-09-05 17:57:58 +0900

    fix user index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,12 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+		
   end
+
+	def history
+		@history = @user
+	end
 
   # GET /users/new
   def new
@@ -40,15 +45,12 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+		new_property = user_params[:property]
+		if @user.fix_property(new_property)
+			redirect_to @user, notice: 'User was successfully updated.'
+		else
+			render :edit
+		end
   end
 
   # DELETE /users/1
@@ -69,6 +71,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params[:user]
+			params[:user]
     end
 end
